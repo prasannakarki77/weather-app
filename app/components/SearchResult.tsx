049580c1430214194f504468f45b0c8c4088dce8
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-04 18:43:31 +0545

    added handleClick function for cities

--- app/components/SearchResult.tsx
+++ app/components/SearchResult.tsx
@@ -1,11 +1,26 @@
+import { useContext } from "react";
+import { WeatherContext } from "../context/WeatherContext";
+
 interface SearchResultProps {
   place: string;
   code: string;
+  lat: number;
+  lon: number;
 }
 
-const SearchResult: React.FC<SearchResultProps> = ({ place, code }) => {
+const SearchResult: React.FC<SearchResultProps> = ({
+  place,
+  code,
+  lat,
+  lon,
+}) => {
+  const { getLocationWeather } = useContext(WeatherContext);
+  const handleCityClick = () => getLocationWeather(lat, lon, place);
   return (
-    <div className="w-full p-3 border border-[#1E213A] hover:border-gray-400 transition-all hover:cursor-pointer">
+    <div
+      className="w-full p-3 border border-[#1E213A] hover:border-gray-400 transition-all hover:cursor-pointer"
+      onClick={handleCityClick}
+    >
       {place}, {code}
     </div>
   );
