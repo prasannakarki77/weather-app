9ecda4d67c8dbe07f97421f6e691c36b8d189835
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-06 14:16:52 +0545

    added type definition for weather forecast

--- app/types/WeatherContextType.ts
+++ app/types/WeatherContextType.ts
@@ -1,36 +1,97 @@
 import { Dispatch, SetStateAction } from "react";
 
-export interface Wind {
-  speed: number;
-  degree: number;
-}
-
-export interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
-
 export interface Current {
-  wind: Wind | null;
-  humidity: number | null;
-  visibility: number | null;
-  pressure: number | null;
-  weather: Weather[];
-  temp: string;
+  last_updated_epoch: number;
+  last_updated: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: {
+    text: string;
+    icon: string;
+    code: number;
+  };
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
+}
+
+export interface Location {
+  name: string;
+  region: string;
+  country: string;
+  lat: string;
+  lon: string;
+  tz_id: string;
+  localtime_epoch: string;
+  localtime: string;
 }
 
 export interface Forecast {
-  day: string | Date | null;
-  temp: number | null;
+  time_epoch: number;
+  time: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: {
+    text: string;
+    icon: string;
+    code: number;
+  };
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: "S";
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  will_it_rain: number;
+  chance_of_rain: number;
+  will_it_snow: number;
+  chance_of_snow: number;
+  vis_km: number;
+  vis_miles: number;
+  gust_mph: number;
+  gust_kph: number;
+  uv: number;
+}
+
+export interface Weather {
+  current: Current;
+  location: Location;
+  forecast: Forecast[];
 }
 
 export interface WeatherContextProps {
   city: string;
   setCity: Dispatch<SetStateAction<string>>;
-  current: Current | null;
+  weather: Weather | null;
   getCitiesByName: (city: string) => Promise<void>;
   searchResults: string[];
-  getWeatherForecast: (lat: number, lon: number, city: string) => Promise<void>;
+  getWeatherForecast: (city: string) => Promise<void>;
 }
