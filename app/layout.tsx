2bcfd2d4db542d63a0821458f2ed9ab590b153a4
prasannakarki77 <prasanna.karki77@gmail.com>
2023-08-07 22:23:44 +0545

    added raleway font from google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-page-custom-font */
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,6 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
